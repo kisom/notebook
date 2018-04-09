ba617c5bbb0e14e12e4a47b5360bc85f6ed8034e
Kyle Isom <kyle@cloudflare.com>
2018-04-09 09:22:04 -0700

    More tufte updates.

--- index.poly.pm
+++ index.poly.pm
@@ -2,6 +2,10 @@
 
 ◊h1{A Programmer's Notebook}
 
+◊section{
+
 ◊p{Similar to the ◊(link "https://dl.kyleisom.net/" "dev logs"), this
 should be a compendium of various things I've learned about
-programming.}
+programming; it'll be more of a long-form thing.}
+
+}
--- pollen.rkt
+++ pollen.rkt
@@ -1,9 +1,86 @@
 #lang racket/base
 
-(require racket/date txexpr)
-(require pollen/tag)
-(require pollen/decode)
+(require pollen/decode
+         pollen/setup
+         pollen/file
+         txexpr
+         pollen/tag
+         pollen/template
+         pollen/core
+         pollen/pagetree
+         racket/date
+         libuuid)
 
 (provide (all-defined-out))
 
 (define (link url text) `(a [[href ,url]] ,text))
+
+#|
+◊numbered-note, ◊margin-figure, ◊margin-note:
+  These three tag functions produce markup for "sidenotes" in HTML and LaTeX.
+  In our LaTeX template, any hyperlinks also get auto-converted to numbered
+  sidenotes, which is kinda neat. Unfortunately, this also means that when
+  targeting LaTeX, you can't have a hyperlink inside a sidenote since that would
+  equate to a sidenote within a sidenote, which causes Problems.
+  We handle this by not using a normal tag function for hyperlinks. Instead,
+  within these three tag functions we call latex-no-hyperlinks-in-margin to
+  filter out any hyperlinks inside these tags (for LaTeX/PDF only). Then the
+  root function uses a separate decoder to properly handle any hyperlinks that
+  sit outside any of these three tags.
+|#
+
+(define (numbered-note . text)
+    (define refid (uuid-generate))
+    (case (current-poly-target)
+      [(ltx pdf)
+       `(txt "\\footnote{" ,@(latex-no-hyperlinks-in-margin text) "}")]
+      [else
+        `(@ (label [[for ,refid] [class "margin-toggle sidenote-number"]])
+            (input [[type "checkbox"] [id ,refid] [class "margin-toggle"]])
+            (span [(class "sidenote")] ,@text))]))
+
+(define (margin-figure source . caption)
+    (define refid (uuid-generate))
+    (case (current-poly-target)
+      [(ltx pdf)
+       `(txt "\\begin{marginfigure}"
+             "\\includegraphics{" ,source "}"
+             "\\caption{" ,@(latex-no-hyperlinks-in-margin caption) "}"
+             "\\end{marginfigure}")]
+      [else
+        `(@ (label [[for ,refid] [class "margin-toggle"]] 8853)
+            (input [[type "checkbox"] [id ,refid] [class "margin-toggle"]])
+            (span [[class "marginnote"]] (img [[src ,source]]) ,@caption))]))
+
+(define (margin-note . text)
+    (define refid (uuid-generate))
+    (case (current-poly-target)
+      [(ltx pdf)
+       `(txt "\\marginnote{" ,@(latex-no-hyperlinks-in-margin text) "}")]
+      [else
+        `(@ (label [[for ,refid] [class "margin-toggle"]] 8853)
+            (input [[type "checkbox"] [id ,refid] [class "margin-toggle"]])
+            (span [[class "marginnote"]] ,@text))]))
+
+(define (latex-no-hyperlinks-in-margin txpr)
+  ; First define a local function that will transform each ◊hyperlink
+  (define (cleanlinks inline-tx)
+      (if (eq? 'hyperlink (get-tag inline-tx))
+        `(txt ,@(cdr (get-elements inline-tx))
+              ; Return the text with the URI in parentheses
+              " (\\url{" ,(ltx-escape-str (car (get-elements inline-tx))) "})")
+        inline-tx)) ; otherwise pass through unchanged
+  ; Run txpr through the decode-elements wringer using the above function to
+  ; flatten out any ◊hyperlink tags
+  (decode-elements txpr #:inline-txexpr-proc cleanlinks))
+
+; Escape $,%,# and & for LaTeX
+; The approach here is rather indiscriminate; I’ll probably have to change
+; it once I get around to handline inline math, etc.
+(define (ltx-escape-str str)
+  (regexp-replace* #px"([$#%&])" str "\\\\\\1"))
+
+(define (newthought . words)
+  (case (current-poly-target)
+    [(ltx pdf) `(txt "\\newthought{" ,@words "}")]
+    [else `(span [[class "newthought"]] ,@words)]))
