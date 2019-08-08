9aff8544b3e2a739bb75704f0ad9d9ec54e2bf81
Kyle Isom <kyle@imap.cc>
2019-08-08 23:37:59 +0000

    Cleanup set proofs.

--- source/pim/sets.poly.pm
+++ source/pim/sets.poly.pm
@@ -8,46 +8,35 @@ a fundamental skill.}
 
 ◊p{◊strong{Definition 1}: A set is a collection of unique objects.}
 
-◊p{◊strong{Definition 2}: ◊${A \subset B} iff ◊${\{a : a \in A \text{ and } a \in B\}}}
+◊p{◊strong{Definition 2}: A is defined as the subset of B (denoted by
+◊${A \subset B}) iff ◊${\{a : a \in A \text{ and } a \in B\}}.}
 
-◊p{◊strong{Definition 3}: ◊${A \cup B = \{x : x \in A \text{ and } x \in B\}}
-◊margin-note{Note that in maths, ◊${=} implies equivalence, not assignment.}}
+◊p{◊strong{Definition 3}: The union of two sets ◊${A \cup B} is
+defined as ◊${A \cup B = \{x : x \in A \lor x \in B\}}.
+◊margin-note{Note that in maths, ◊${=} implies equivalence, not
+assignment.}}
 
-◊p{◊strong{Definition 4}: ◊${A \cap B = \{x : x \in A \text{ or } x \in B\}}}
-}
+◊p{◊strong{Definition 4}: The intersection of two sets ◊${A \cap B} is
+defined as ◊${A \cap B = \{x : x \in A \land x \in B\}}.}}
 
 ◊section{
-◊p{◊strong{Theorem 1}: ◊${\varnothing} is a subset of all other sets.
+◊p{
+◊strong{Theorem 1}: ◊${\varnothing} is a subset of all other sets. That is,
+◊${\forall A \text{ where } A \text { is a set,} \varnothing \subset A}.
 ◊margin-note{This theorem isn't in the book, but I wanted to prove it to myself
 and to use it as a foundation for the other proofs.}}
-◊p{◊strong{Proof:}
-◊br{}◊br{}
-There are no elements in ◊${\varnothing}, therefore all elements in
-◊${\varnothing} are elements of any other set.}
+
+◊p{◊strong{Proof:} There are no elements in ◊${\varnothing}, therefore
+all elements in ◊${\varnothing} are elements of any other set.}
 ◊p{◊${\square}}
 
 ◊p{◊strong{Theorem 2}: ◊${A \cap B \subset A}}
-◊p{◊strong{Proof}:
-◊br{}◊br{}
-Assume ◊${A} is a set with cardinality 1, e.g. ◊${A = \{a\}}.◊br{}
-Assume ◊${B} is a set with cardinality 0, e.g. ◊${B = \varnothing}.◊br{}
-◊br{}
-By definition 4, ◊${A \cap B = \{a\}}.◊br{}
-◊br{}
-By theorem 1 and definition 2, ◊${A \cap B \subset A}.}
-◊p{◊${\square}}}
+◊p{◊strong{Proof}: Let ◊${C = A \cap B = \{x : x \in A \land x \in B\}}. By
+definition 3, ◊${\forall c \in C, c \in A}. Therefore ◊${C \subset A}.}
+◊p{◊${\square}}
 
-◊section{
 ◊p{◊strong{Theorem 3}: ◊${A \subset A \cup B}}
-◊p{◊strong{Proof}:
-◊br{}◊br{}
-Assume ◊${A} is a set with cardinality 1, e.g. ◊${A = \{a\}}.◊br{}
-Assume ◊${B} is a set with cardinality 0, e.g. ◊${B = \varnothing}.◊br{}
-◊br{}
-By definition 3, ◊${A \cup B = \varnothing}
-◊br{}◊br{}
-By theorem 1 and definition 4, since there are no elements in
-◊${\varnothing}, every element in ◊${A \cup B} appears in
-◊${A}. Therefore, ◊${A \subset A \cup B}.}
-
-◊p{◊${\square}}}
+◊p{◊strong{Proof}: Let ◊${C = A \cup B = \{x : x \in A \lor x \in B\}}. By
+definition, ◊${C} contains every element in ◊${A} and therefore ◊${A \subset C}.}
+◊p{◊${\square}}
+}
