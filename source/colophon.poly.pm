1cf14df15c29c78432c0d4bce091ffcbc97b8ccd
Kyle Isom <kyle@imap.cc>
2019-08-08 23:53:16 +0000

    Update colophon.

--- source/colophon.poly.pm
+++ source/colophon.poly.pm
@@ -7,10 +7,14 @@
 the ◊(link "https://edwardtufte.github.io/tufte-css/" "Tufte CSS")
 styleguide adapted for Pollen.}
 
-◊p{The fonts used are ◊(link
-"https://typographyforlawyers.com/equity.html" "Equity") for the serif
-text and ◊(link "https://typographyforlawyers.com/triplicate.html"
-"Triplicate") for code listings and monospace.}
+◊p{The fonts used are ◊(link "https://typographyforlawyers.com/equity.html" "Equity")
+for the serif text and ◊(link "https://typographyforlawyers.com/triplicate.html""Triplicate")
+for code listings and monospace.}
 
 ◊p{Notably, Pollen and both fonts are products of ◊(link
-"https://typographyforlawyers.com/about.html" "Matthew Butterick").}}
+"https://typographyforlawyers.com/about.html" "Matthew Butterick").}
+
+◊p{◊(link "https://www.mathjax.org/" "MathJax") is used for maths
+notation support.}
+
+◊p{The source code for this is on ◊(link "https://gitlab.com/kisom/notebook" "Gitlab").}}
