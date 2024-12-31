a60fa878f4b4d9738cd285268ad5664852ee6bc2
Kyle Isom <kyle@imap.cc>
2024-12-31 12:26:44 -0800

    update devlog note

--- source/index.poly.pm
+++ source/index.poly.pm
@@ -3,7 +3,7 @@
 ◊h1{A Programmer's Notebook}
 
 ◊section{◊p{
-Similar to the ◊(link "https://dl.kyleisom.net/" "dev logs"), this
+Similar to the ◊(link "https://log.wntrmute.dev/" "dev logs"), this
 should be a compendium of various things I've learned about
 programming; it'll be more of a long-form thing.}}
 
